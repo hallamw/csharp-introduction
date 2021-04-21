724153659d61edbcade5773a4808e6e5a6cf0a36
hallamw <hallam@how.co.nz>
2021-04-21 13:22:34 +1200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hellomars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = args[0];
+            Console.WriteLine("Hello Mars! from " + myName);
+        }
+    }
+}
